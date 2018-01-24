2f807245cddc1c0c94ef5ed60932505795408678
Michael McAndrew <michael@corribdigital.com>
2018-01-24 19:50:45 +0000

    added insertion sort and array print

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# sorting_algorithms
+# Sorting Algorithms
+
+A collection of optimised sorting algorithms written in C.
---
+++ print_arr.c
@@ -0,0 +1,13 @@
+/**
+ * C program for printing an array
+ */
+
+#include <stdio.h>
+
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; i++) {
+		printf("%d, ", arr[i]);
+	}
+	printf("\n");
+}
